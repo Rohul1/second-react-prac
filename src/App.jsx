03e5795b008920ed16d12fd730424f248c726263
Rohul1 <rohul669319@gmail.com>
2023-12-12 16:49:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <District></District>
+    </>
+  )
+}
+function District (){
+  return(
+    <div>
+      <h2>Name : </h2>
+    <p>Speciality :</p>
+    </div>
+  )
+}
+
+
+export default App
