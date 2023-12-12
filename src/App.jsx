723b70e6b49b43b94d4498834ef0b7e6f4869105
Rohul1 <rohul669319@gmail.com>
2023-12-12 20:43:38 +0600

    added btn

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,28 @@ function App() {
 const districtStyle = {
 backgroundColor : "lightblue",
 margin :"20px",
+borderRadius : "20px",
+alignItem : "center",
+padding : "20px"
 
 
 }
 
 
 function District (props){
+const [power,setPower] = useState(1)
+
+const bostPower = ()=>{
+  const newPower = power * 2;
+  setPower(newPower);
+}
+
   return(
     <div style={districtStyle}>
       <h2>Name : {props.name}</h2>
     <p>Speciality : {props.special}</p>
+    <h4>Power : {power}</h4>
+    <button onClick={bostPower}>Bost The Power</button>
     </div>
   )
 }
